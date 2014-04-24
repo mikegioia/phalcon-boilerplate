2353261e6a82348ddd2e7de43b416ae4de758360
Mike Gioia <mikegioia@gmail.com>
2014-04-23 21:30:08 -0400

    changed installer to work from any location. added ability to add sql password from install script.

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,17 +4,21 @@
 # creates local configuration files and sets up the environment
 
 # read in the env
-#
 usage="Sets up the Phalcon Boilerplate application
 usage:
     $(basename "$0") [-h] <profile>
 options:
     -h  show this help text"
 
-while getopts ':h' option; do
+# whether to force config update
+force=0
+
+while getopts ':hf' option; do
   case "$option" in
     h) echo "$usage"
        exit
+       ;;
+    f) force=1
        ;;
    \?) printf "Illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -24,21 +28,29 @@ while getopts ':h' option; do
 done
 shift $((OPTIND - 1))
 
+# get the paths
+rootpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+apppath="$rootpath/../app";
 # copy the config environment file
-#
 env=${1:-"local"}
 
-if [ ! -f "../app/etc/env/${env}.php" ] ; then
+if [ ! -f "${apppath}/etc/env/${env}.php" ] ; then
     printf "Invalid environment specified: %s\n" "$env" >&2
     exit 1
 fi
 
-if [ ! -f "../app/etc/config.local.php" ] ; then
+if [[ ! -f "${apppath}/etc/config.local.php" || $force == 1 ]] ; then
     echo "Copying default '${env}' config file"
-    cp ../app/etc/env/${env}.php ../app/etc/config.local.php
+    cp ${apppath}/etc/env/${env}.php ${apppath}/etc/config.local.php
 else
-    echo "'${env}' config file exists, skipping!"
+    echo "'${env}' config file exists, skipping! (try -f to force)"
 fi
+
+# prompt to update the SQL password
+read -s -p "Enter SQL Password: " sqlReplace
+echo ''
+sqlFind="##SQLPASSWORD##"
+sed -i "s/${sqlFind}/${sqlReplace}/g" ${apppath}/etc/config.local.php
 
 echo ''
 echo 'Done! Make sure you run the SQL statements.'
